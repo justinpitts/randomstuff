94a8125c137aecbbaf4ffbde6992a6cb4d80f57b
justinpitts <justinpitts@01943acd-c019-0410-87af-872aabc633d8>
2006-08-22 22:43:04 +0000

    prettied up the temp file logic
    git-svn-id: http://randomstuff.googlecode.com/svn/trunk@27 01943acd-c019-0410-87af-872aabc633d8

--- svnindex/src/com/randomhumans/svnindex/ContentDocument.java
+++ svnindex/src/com/randomhumans/svnindex/ContentDocument.java
@@ -1,13 +1,13 @@
+
 package com.randomhumans.svnindex;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
-import java.util.UUID;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.tmatesoft.svn.core.SVNDirEntry;
@@ -16,15 +16,20 @@ import org.tmatesoft.svn.core.SVNNodeKind;
 import org.tmatesoft.svn.core.io.SVNRepository;
 
 public class ContentDocument
-{    
+{
     private static final String CONTENT = "content";
+
     private static final String URL = "url";
+
     private static final String REVISION = "revision";
+
     private static final String DATE = "date";
+
     private static final String MESSAGE = "message";
+
     private static final String AUTHOR = "author";
 
-    public static Document createDocument(SVNDirEntry entry, String path)
+    public static Document createDocument(SVNDirEntry entry, String path) throws IOException
     {
         Document doc = new Document();
         try
@@ -34,31 +39,42 @@ public class ContentDocument
         }
         catch (SVNException e1)
         {}
-        
+
         doc.add(new Field(AUTHOR, entry.getAuthor() + "", Field.Store.YES, Field.Index.TOKENIZED));
         doc.add(new Field(MESSAGE, entry.getCommitMessage() + "", Field.Store.COMPRESS, Field.Index.TOKENIZED));
         doc.add(new Field(DATE, entry.getDate().toString(), Field.Store.YES, Field.Index.NO));
         doc.add(new Field(REVISION, Long.toString(entry.getRevision()), Field.Store.YES, Field.Index.UN_TOKENIZED));
         doc.add(new Field(URL, entry.getURL().toString(), Field.Store.YES, Field.Index.NO));
-        
+
         System.out.println(entry.getURL());
-        
+
         try
         {
             SVNRepository repo = RepositoryHelper.getRepo(entry.getURL().toString());
             try
             {
-                File temp =  getTempFile();
-                OutputStream stream = new FileOutputStream(temp);                    
-                repo.getFile("", -1, null, stream);
+                File temp = File.createTempFile("index", "dat");
+                OutputStream stream = new FileOutputStream(temp);
+                try
+                {
+                    repo.getFile("", -1, null, stream);
+                }
+                finally
+                {
+                    stream.close();
+                }
                 Reader r = new InputStreamReader(new FileInputStream(temp));
-                doc.add(new Field(CONTENT, r));
+                try
+                {
+                    doc.add(new Field(CONTENT, r));
+                }
+                finally
+                {
+                    r.close();
+                    temp.delete();
+                }
             }
-            catch (FileNotFoundException e)
-            {
-                // TODO Auto-generated catch block -- Finish Me
-                e.printStackTrace();
-            } finally
+            finally
             {
                 repo.closeSession();
             }
@@ -71,11 +87,4 @@ public class ContentDocument
         doc.add(new Field(CONTENT, "", Field.Store.NO, Field.Index.TOKENIZED));
         return doc;
     }
-    
-    private static File getTempFile()
-    {
-        String folder = System.getProperty("java.io.tmpdir");
-        return new File(folder, UUID.randomUUID().toString());
-    }
-
 }
