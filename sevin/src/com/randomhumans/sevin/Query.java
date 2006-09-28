b8ecda3f105f807a59c1f7d4df6f7e779255af96
justinpitts <justinpitts@01943acd-c019-0410-87af-872aabc633d8>
2006-09-28 03:40:44 +0000

    added / refactored the query classes
    git-svn-id: http://randomstuff.googlecode.com/svn/trunk@38 01943acd-c019-0410-87af-872aabc633d8

--- sevin/src/com/randomhumans/sevin/Query.java
+++ sevin/src/com/randomhumans/sevin/Query.java
@@ -3,16 +3,15 @@ package com.randomhumans.sevin;
 import java.io.IOException;
 import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.Sort;
-
-import com.randomhumans.svnindex.indexing.RevisionDocument;
+import com.randomhumans.svnindex.indexing.ContentDocument;
+import com.randomhumans.svnindex.queries.ContentQuery;
 import com.randomhumans.svnindex.queries.IQuery;
-import com.randomhumans.svnindex.queries.RevisionQuery;
 
 public class Query
 {
     private String query = "";
     
-    private String sortField = RevisionDocument.REVISION_FIELDNAME;
+    private String sortField = ContentDocument.URL;
     
     public String getQuery()
     {
@@ -26,7 +25,7 @@ public class Query
 
     public Document[] getResults()
     {
-        IQuery cq = new RevisionQuery();
+        IQuery cq = new ContentQuery();
         try
         {
         Hits h = cq.performQuery(query, new Sort(sortField));
