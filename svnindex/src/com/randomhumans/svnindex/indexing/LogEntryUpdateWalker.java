123eb6c7a3b8d09a96a8d2759a899e19c54aa26c
justinpitts <justinpitts@01943acd-c019-0410-87af-872aabc633d8>
2007-02-19 19:58:45 +0000

    implementing a new walker to update the index based on new log entries in svn
    git-svn-id: http://randomstuff.googlecode.com/svn/trunk@61 01943acd-c019-0410-87af-872aabc633d8

--- svnindex/src/com/randomhumans/svnindex/indexing/LogEntryUpdateWalker.java
+++ svnindex/src/com/randomhumans/svnindex/indexing/LogEntryUpdateWalker.java
@@ -1,6 +1,37 @@
+
 package com.randomhumans.svnindex.indexing;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+
+import com.randomhumans.svnindex.util.RepositoryHelper;
 
 public class LogEntryUpdateWalker
 {
+    HashSet<String> visitedUrls = new HashSet<String>();
+
+    public void walk() throws IOException, SVNException
+    {
+        IndexInfo info = IndexInfo.loadFromIndex();
+        long endRevision = info.getRevision() + 1;
+        long currentRevision = RepositoryHelper.getLatestRevision();
+
+        for (long i = currentRevision; i > endRevision; i--)
+        {
+            SVNLogEntry logEntry = RepositoryHelper.getLogEntry(i);
+            for (Object o : logEntry.getChangedPaths().keySet())
+            {
+                String url = o.toString();
+                if (!visitedUrls.contains(url))
+                {
+                 //   ContentIndexerThread.queueDocument(d)
+                    visitedUrls.add(o.toString());
+                }
+            }
+        }
+    }
 
 }
