98bd0ae476c65f0c1e50c800f8e0aee87a3b4411
justinpitts <justinpitts@01943acd-c019-0410-87af-872aabc633d8>
2007-03-09 14:37:00 +0000

    friendlyify walker
    git-svn-id: http://randomstuff.googlecode.com/svn/trunk@79 01943acd-c019-0410-87af-872aabc633d8

--- svnindex/src/com/randomhumans/svnindex/indexing/Walker.java
+++ svnindex/src/com/randomhumans/svnindex/indexing/Walker.java
@@ -27,11 +27,12 @@ public class Walker
     public static void main(final String[] args) throws IOException, SVNException
     {
         Walker.log.info(Configuration.getConfig().toString());        
-        final IndexMetaDocument info = IndexMetaDocument.loadFromIndex(Configuration.getConfig().getIndexLocation());
+        final IndexMetaDocument info = getMetaDoc();
         final boolean rebuild = ((args.length > 0) && args[0].equalsIgnoreCase("--REBUILD")) ;
-        IFilter<String, SVNDirEntry> filter;
+        IFilter<String, SVNDirEntry> filter;        
         if (rebuild)
         {
+            
             filter = new DefaultNameAndSizeFilter(Configuration.getConfig().getIgnoredNames());
             Walker.log.debug("rebuild mode");
         }
@@ -40,6 +41,11 @@ public class Walker
             filter = new NameSizeAndRevisionFilter(Configuration.getConfig().getIgnoredNames(), info.getRevision() + 1);
             Walker.log.debug("update mode");
         }
+        updateIndex(info, rebuild, filter);
+    }
+
+    private static void updateIndex(final IndexMetaDocument info, final boolean rebuild, IFilter<String, SVNDirEntry> filter) throws IOException, SVNException
+    {
         ContentIndexerThread.init(rebuild);
         final SVNRepoTreeWalker walker = new SVNRepoTreeWalker();
         info.setRevision(walker.map(Configuration.getConfig().getFolders(),filter));
@@ -47,5 +53,49 @@ public class Walker
         ContentIndexerThread.shutdown();
         info.save(Configuration.getConfig().getIndexLocation());
         Walker.log.info("shutdown complete");
+    }
+
+    private static IndexMetaDocument getMetaDoc() throws IOException
+    {
+        final IndexMetaDocument info = IndexMetaDocument.loadFromIndex(Configuration.getConfig().getIndexLocation());
+        return info;
+    }
+    
+    public static void rebuild()
+    {
+        IFilter<String, SVNDirEntry> filter = new DefaultNameAndSizeFilter(Configuration.getConfig().getIgnoredNames());
+        Walker.log.debug("rebuild mode");
+        try
+        {
+            updateIndex(getMetaDoc(), true, filter);
+        }
+        catch (IOException e)
+        {
+            log.error(e,e);
+        }
+        catch (SVNException e)
+        {
+            log.error(e,e);
+         }       
+    }
+    
+    public static void update() throws IOException
+    {
+        IndexMetaDocument info = getMetaDoc();
+        IFilter<String, SVNDirEntry> filter = new NameSizeAndRevisionFilter(Configuration.getConfig().getIgnoredNames(), info.getRevision() + 1);
+        Walker.log.debug("update mode");
+        try
+        {
+            updateIndex(info, false, filter);
+        }
+        catch (IOException e)
+        {
+            log.error(e,e);
+        }
+        catch (SVNException e)
+        {
+            log.error(e,e);
+         }       
+        
     }
 }
